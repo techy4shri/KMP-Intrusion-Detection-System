53f718ed734337eca12a6cfafa289a62988eb8d4
techy4shri <sushri4tech@gmail.com>
2024-12-23 21:20:05 +0530

    setting up

---
+++ DISCORD/main.py
@@ -0,0 +1,17 @@
+import discord
+from discord.ext import commands
+
+intents = discord.Intents.default()
+intents.message_content = True
+
+bot = commands.Bot(command_prefix='!', intents=intents)
+
+@bot.event
+async def on_ready():
+    print(f'We have logged in as {bot.user}')
+
+@bot.command()
+async def hello(ctx):
+    await ctx.send('Hello!')
+
+bot.run('YOUR_BOT_TOKEN')
